ea9f2f4655e4da0ebd81d8f23d519487c4a1ae07
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-08-16 15:55:10 +0545

    Removed path addition in sys from wsgi server

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -8,13 +8,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
-
-path = '/home/kiran/Documents/Development/core/SACCHAI/Backend'
-if path not in sys.path:
-    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
