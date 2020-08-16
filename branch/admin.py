46f81d520db6bcd5cd9205eba21326ebef6280d0
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-08-16 20:17:38 +0545

    Added branch admin

--- branch/admin.py
+++ branch/admin.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 
-from branch.models import Member
+from branch.models import Member, Branch
 
 
 class MemberAdmin(UserAdmin):
     list_display = (
         "name", "email", "address", "phone",
-        "country", "district", "is_admin",
-        "created_by", "approved_by"
+        "country", "district", "is_staff", "is_approved",
+        "approved_by", "created_at", "updated_at"
     )
-    list_filter = ("address", "is_active", "state")
+    list_filter = ("address", "is_active", "is_approved", "country", "district")
 
     fieldsets = (
         ("Authentication Information", {
@@ -25,7 +25,7 @@ class MemberAdmin(UserAdmin):
 
         ("Business Details", {
             "classes": ("wide",),
-            "fields": ("state", "is_active", "created_by", "updated_by", "approved_by")
+            "fields": ("is_staff", "is_approved", "is_active", "approved_by")
         })
     )
 
@@ -40,7 +40,7 @@ class MemberAdmin(UserAdmin):
         }),
         ("Business Information", {
             "classes": ("wide",),
-            "fields": ("is_staff", "is_admin")
+            "fields": ("is_staff",)
         })
     )
 
@@ -48,12 +48,46 @@ class MemberAdmin(UserAdmin):
     date_hierarchy = "created_at"
     ordering = (
         "name", "email", "address", "phone",
-        "is_active", "state", "is_active",
-        "created_by", "approved_by"
+        "country", "district", "is_staff", "is_approved",
+        "approved_by", "created_at", "updated_at"
     )
 
     filter_horizontal = ()
 
 
+class BranchAdmin(admin.ModelAdmin):
+    list_display = (
+        "name", "address", "phone", "country", "district", "is_main",
+        "created_by", "created_at", "updated_by", "updated_at"
+    )
+
+    fieldsets = (
+        ("Branch Information", {
+            "classes": ("wide",),
+            "fields": ("name", "address", "phone", "country", "district", "is_main")
+        }),
+    )
+
+    search_fields = ("name", "address", "phone", "district")
+    list_filter = ("is_main", "country")
+    date_hierarchy = "created_at"
+    ordering = ("name", "address", "phone", "country", "district", "is_main")
+
+    def save_model(self, request, obj, form, change):
+        obj.created_by = request.user
+        obj.updated_by = request.user
+        obj.save()
+
+    def save_formset(self, request, form, formset, change):
+        if formset.model == Branch:
+            instances = formset.save(commit=False)
+            for instance in instances:
+                instance.updated_by = request.user
+                instance.save()
+        else:
+            formset.save()
+
+
 admin.site.register(Member, MemberAdmin)
+admin.site.register(Branch, BranchAdmin)
 admin.site.unregister(Group)
