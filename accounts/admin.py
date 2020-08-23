daba12cfbf74cc41c773593d02b8799674a733f3
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-08-23 23:59:36 +0545

    Modified member to extend from django user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,60 +1,77 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 from accounts.models import Member
 
 
-class MemberAdmin(UserAdmin):
-    list_display = (
-        "name", "branch",
-        "district", "is_staff", "is_approved",
-        "approved_by", "created_at", "updated_at"
-    )
-    list_filter = ("address", "is_active", "is_approved", "country", "branch")
+class MemberInline(admin.StackedInline):
+    model = Member
+    fk_name = "user"
+    can_delete = False
+    extra = 0
+    verbose_name_plural = "Add Branch Member Detail"
+
+    exclude = ("approved_by", "approved_at")
 
     # update form for admin site
     fieldsets = (
-        ("Authentication Information", {
+        ("Business Info", {
             "classes": ("wide", "extrapretty"),
-            "fields": ("email", "password")
+            "fields": ("address", "phone", "country", "district", "branch", "is_approved")
         }),
-        ("Personal Information.", {
-            "classes": ("wide", "extrapretty"),
-            "fields": ("name", "address", "phone", "country", "district")
-        }),
+    )
 
-        ("Business Details", {
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (MemberInline,)
+
+    list_display = (
+        "username", "email", "first_name", "last_name",
+        "is_superuser", "is_staff", "date_joined"
+    )
+
+    def save_formset(self, request, form, formset, change):
+        if formset.model == Member:
+            instances = formset.save(commit=False)
+            for instance in instances:
+                if instance.is_approved:
+                    instance.approved_by = request.user
+                instance.save()
+        else:
+            formset.save()
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = (
+        "user", "address", "country", "district", "phone",
+        "branch", "is_approved", "approved_by", "approved_at",
+    )
+    list_filter = (
+        ("is_approved", admin.BooleanFieldListFilter),
+        ("branch", admin.RelatedFieldListFilter),
+    )
+    search_fields = ("user", "address", "phone")
+
+    fieldsets = (
+        ("Personal Info", {
             "classes": ("wide", "extrapretty"),
-            "fields": ("branch", "is_staff", "is_approved", "is_active")
+            "fields": (
+                "user", "address", "phone", "country", "district"
+            )
+        }),
+        ("Business Info", {
+            "classes": ("wide", "extrapretty"),
+            "fields": (
+                "branch", "is_approved"
+            )
         })
     )
 
-    # create form for admin site
-    add_fieldsets = (
-        ("Authentication Information", {
-            "classes": ("wide", "extrapretty"),
-            "fields": ("email", "password1", "password2"),
-        }),
-        ("Personal Information", {
-            "classes": ("wide", "extrapretty"),
-            "fields": ("name", "address", "phone", "country", "district")
-        }),
-        ("Business Information", {
-            "classes": ("wide", "extrapretty"),
-            "fields": ("branch", "is_staff",)
-        })
-    )
-
-    search_fields = ("email", "name", "address", "country", "district")
-    date_hierarchy = "created_at"
     ordering = (
-        "name", "branch",
-        "district", "is_staff", "is_approved",
-        "approved_by", "created_at", "updated_at"
+        "user", "address", "country", "district", "phone",
+        "branch", "is_approved", "approved_by", "approved_at",
     )
-
-    filter_horizontal = ()
 
     def save_model(self, request, obj, form, change):
         if obj.is_approved:
@@ -72,5 +89,6 @@ class MemberAdmin(UserAdmin):
             formset.save()
 
 
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Member, MemberAdmin)
-admin.site.unregister(Group)
