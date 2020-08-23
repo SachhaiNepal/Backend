9fd8011a37bd135ff79cb017ac6ddcbe561dd022
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-08-23 23:59:36 +0545

    Modified member to extend from django user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import User
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -6,33 +6,8 @@ from branch.models import Branch
 from utils.choices import COUNTRY_CHOICES, DISTRICT_CHOICES
 
 
-class MemberManager(BaseUserManager):
-    use_in_migrations = True
-
-    def __create_user(self, email, password, is_staff, **kwargs):
-        if not email:
-            raise ValueError("email address field is required!")
-        if not password:
-            raise ValueError("password field is required!")
-
-        email = self.normalize_email(email)
-        member = self.model(
-            email=email,
-            is_staff=is_staff,
-            **kwargs
-        )
-        member.set_password(password)
-        member.save(using=self._db)
-        return member
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        user = self.__create_user(email, password, True, **extra_fields)
-        return user
-
-
-class Member(AbstractBaseUser):
-    name = models.CharField(max_length=255, unique=True, blank=True, null=True)
-    email = models.EmailField(unique=True)
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=512, blank=True, null=True)
     country = models.CharField(max_length=3, choices=COUNTRY_CHOICES, null=True, blank=True)
     district = models.CharField(max_length=14, choices=DISTRICT_CHOICES, null=True)
@@ -46,12 +21,10 @@ class Member(AbstractBaseUser):
         blank=True
     )
 
-    is_staff = models.BooleanField(default=False, verbose_name="Is Admin")
-    is_active = models.BooleanField(default=True)
     is_approved = models.BooleanField(default=False)
 
     approved_by = models.ForeignKey(
-        "self",
+        User,
         null=True,
         blank=True,
         on_delete=models.DO_NOTHING,
@@ -59,24 +32,8 @@ class Member(AbstractBaseUser):
     )
     approved_at = models.DateTimeField(default=None, null=True, blank=True)
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    objects = MemberManager()
-
-    USERNAME_FIELD = "email"
-
-    # REQUIRED_FIELDS = [
-    # ]
-
     class Meta:
         verbose_name_plural = "Members"
 
     def __str__(self):
-        return self.email
-
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, package_name):
-        return True
+        return self.user.username
