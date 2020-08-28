bf79541b7bb06a8de0ccf3dfbdfa664d528ffa97
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-08-29 00:37:25 +0545

    Added multimedia API

--- multimedia/urls.py
+++ multimedia/urls.py
@@ -1,6 +1,19 @@
-from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    # to be added
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from multimedia.views import MultimediaViewSet, MultimediaVideoViewSet, \
+    MultimediaAudioViewSet, ArticleViewSet, ArticleImageViewSet
+
+router = DefaultRouter()
+router.register(r"multimedia", MultimediaViewSet, basename="multimedia")
+router.register(r"video", MultimediaVideoViewSet, basename="video")
+router.register(r"audio", MultimediaAudioViewSet, basename="audio")
+router.register(r"article", ArticleViewSet, basename="article")
+router.register(r"image", ArticleImageViewSet, basename="image")
+urlpatterns = router.urls
+urlpatterns += [
+    #
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- multimedia/views.py
+++ multimedia/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets, status
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from multimedia.models import Multimedia, MultimediaVideo, MultimediaAudio, Article, ArticleImage
+from multimedia.serializers import MultimediaSerializer, MultimediaVideoSerializer, \
+    MultimediaAudioSerializer, ArticleSerializer, ArticleImageSerializer
+
+
+class MultimediaViewSet(viewsets.ModelViewSet):
+    queryset = Multimedia.objects.all()
+    serializer_class = MultimediaSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class MultimediaVideoViewSet(viewsets.ModelViewSet):
+    queryset = MultimediaVideo.objects.all()
+    serializer_class = MultimediaVideoSerializer
+    permission_classes = [permissions.IsAdminUser]
+    parser_classes = (MultiPartParser, FormParser)
+
+
+class MultimediaAudioViewSet(viewsets.ModelViewSet):
+    queryset = MultimediaAudio.objects.all()
+    serializer_class = MultimediaAudioSerializer
+    permission_classes = [permissions.IsAdminUser]
+    parser_classes = (MultiPartParser, FormParser)
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class ArticleImageViewSet(viewsets.ModelViewSet):
+    queryset = ArticleImage.objects.all()
+    serializer_class = ArticleImageSerializer
+    permission_classes = [permissions.IsAdminUser]
+    parser_classes = (MultiPartParser, FormParser)
+
