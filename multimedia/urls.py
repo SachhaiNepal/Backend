b4030e0e2a5eb83dc8b2435872357062b226c3f0
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-08-30 13:38:31 +0545

    Added token auth with list image upload in article create API

--- multimedia/urls.py
+++ multimedia/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from multimedia.views import MultimediaViewSet, MultimediaVideoViewSet, \
-    MultimediaAudioViewSet, ArticleViewSet, ArticleImageViewSet
+    MultimediaAudioViewSet, ArticleViewSet, ArticleImageViewSet, CreateArticleWithImageList
 
 router = DefaultRouter()
 router.register(r"multimedia", MultimediaViewSet, basename="multimedia")
@@ -13,6 +14,6 @@ router.register(r"article", ArticleViewSet, basename="article")
 router.register(r"image", ArticleImageViewSet, basename="image")
 urlpatterns = router.urls
 urlpatterns += [
-    #
+    path("article-image", CreateArticleWithImageList.as_view()),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- multimedia/views.py
+++ multimedia/views.py
@@ -1,11 +1,12 @@
 from rest_framework import permissions, viewsets, status
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from multimedia.models import Multimedia, MultimediaVideo, MultimediaAudio, Article, ArticleImage
 from multimedia.serializers import MultimediaSerializer, MultimediaVideoSerializer, \
-    MultimediaAudioSerializer, ArticleSerializer, ArticleImageSerializer
+    MultimediaAudioSerializer, ArticleSerializer, ArticleImageSerializer, ArticleWithImageListSerializer
 
 
 class MultimediaViewSet(viewsets.ModelViewSet):
@@ -40,3 +41,18 @@ class ArticleImageViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAdminUser]
     parser_classes = (MultiPartParser, FormParser)
 
+
+class CreateArticleWithImageList(APIView):
+    authentication_classes = [TokenAuthentication]
+    parser_classes = (MultiPartParser, FormParser,)
+
+    def post(self, request):
+        if request.user.is_anonymous:
+            return Response({
+                "details": "User must be logged in.",
+            }, status=status.HTTP_403_FORBIDDEN)
+        serializer = ArticleWithImageListSerializer(data=request.data, context={"request": request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg": "success"})
+        return Response(serializer.errors)
