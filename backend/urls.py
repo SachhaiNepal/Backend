9be2b10d7ccccb3b8618d1dd9aa30b0380aaa942
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-09-07 16:38:12 +0545

    Typo fixed

--- backend/urls.py
+++ backend/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path("load-countries", load_countries),
     path("load-provinces", load_provinces),
     path("load-districts", load_districts_of_nepal),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,})
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
