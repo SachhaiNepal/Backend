25fe1bc425a2d8ca9190b54d405d025908c1e32a
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-09-13 01:57:53 +0545

    Modified imports

--- multimedia/views/views.py
+++ multimedia/views/views.py
@@ -2,44 +2,44 @@ from rest_framework import permissions, viewsets
 from rest_framework.parsers import FormParser, MultiPartParser
 
 from multimedia import models
-from multimedia import serializers
+from multimedia.serializers.model_serializer import MultimediaSerializer, ArticleSerializer
 
 
 class MultimediaViewSet(viewsets.ModelViewSet):
     queryset = models.Multimedia.objects.all()
-    serializer_class = serializers.MultimediaSerializer
+    serializer_class = MultimediaSerializer
     # permission_classes = [permissions.IsAdminUser]
 
 
-class MultimediaVideoViewSet(viewsets.ModelViewSet):
-    queryset = models.MultimediaVideo.objects.all()
-    serializer_class = serializers.MultimediaVideoSerializer
-    # permission_classes = [permissions.IsAdminUser]
-    parser_classes = (MultiPartParser, FormParser)
-
-
-class MultimediaAudioViewSet(viewsets.ModelViewSet):
-    queryset = models.MultimediaAudio.objects.all()
-    serializer_class = serializers.MultimediaAudioSerializer
-    # permission_classes = [permissions.IsAdminUser]
-    parser_classes = (MultiPartParser, FormParser)
-
-
-class MultimediaImageViewSet(viewsets.ModelViewSet):
-    queryset = models.MultimediaImage.objects.all()
-    serializer_class = serializers.MultimediaImageSerializer
-    # permission_classes = [permissions.IsAdminUser]
-    parser_classes = (MultiPartParser, FormParser)
+# class MultimediaVideoViewSet(viewsets.ModelViewSet):
+#     queryset = models.MultimediaVideo.objects.all()
+#     serializer_class = MultimediaVideoSerializer
+#     permission_classes = [permissions.IsAdminUser]
+#     parser_classes = (MultiPartParser, FormParser)
+#
+#
+# class MultimediaAudioViewSet(viewsets.ModelViewSet):
+#     queryset = models.MultimediaAudio.objects.all()
+#     serializer_class = MultimediaAudioSerializer
+#     permission_classes = [permissions.IsAdminUser]
+#     parser_classes = (MultiPartParser, FormParser)
+#
+#
+# class MultimediaImageViewSet(viewsets.ModelViewSet):
+#     queryset = models.MultimediaImage.objects.all()
+#     serializer_class = MultimediaImageSerializer
+#     permission_classes = [permissions.IsAdminUser]
+#     parser_classes = (MultiPartParser, FormParser)
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = models.Article.objects.all()
-    serializer_class = serializers.ArticleSerializer
+    serializer_class = ArticleSerializer
     # permission_classes = [permissions.IsAdminUser]
 
 
-class ArticleImageViewSet(viewsets.ModelViewSet):
-    queryset = models.ArticleImage.objects.all()
-    serializer_class = serializers.ArticleImageSerializer
-    # permission_classes = [permissions.IsAdminUser]
-    parser_classes = (MultiPartParser, FormParser)
+# class ArticleImageViewSet(viewsets.ModelViewSet):
+#     queryset = models.ArticleImage.objects.all()
+#     serializer_class = ArticleImageSerializer
+#     permission_classes = [permissions.IsAdminUser]
+#     parser_classes = (MultiPartParser, FormParser)
