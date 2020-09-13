975cdcec54530887bb402a6f8d24ab8c7fb2c850
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-09-13 23:20:49 +0545

    Added media detail view with delete APIs

--- multimedia/urls.py
+++ multimedia/urls.py
@@ -3,8 +3,10 @@ from django.conf import settings
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from multimedia.views.getters import *
-from multimedia.views.list_uploader import *
+from multimedia.views.media_detail import *
+from multimedia.views.multimedia import *
+from multimedia.views.article import *
+from multimedia.views.list_upload import *
 from multimedia.views import views
 
 router = DefaultRouter()
@@ -16,11 +18,15 @@ router.register(r"article", views.ArticleViewSet, basename="article")
 # router.register(r"article/image", views.ArticleImageViewSet, basename="article-image")
 urlpatterns = router.urls
 urlpatterns += [
-    path("create-article", CreateArticleWithImageList.as_view()),
-    path("create-multimedia", CreateMultimediaWithMultimediaList.as_view()),
+    path("create/article", CreateArticleWithImageList.as_view()),
+    path("create/multimedia", CreateMultimediaWithMultimediaList.as_view()),
     path("article/<int:pk>/image", ListArticleImages.as_view()),
     path("multimedia/<int:pk>/video", ListMultimediaVideos.as_view()),
     path("multimedia/<int:pk>/audio", ListMultimediaAudios.as_view()),
-    path("multimedia/<int:pk>/image", ListMultimediaImages.as_view())
+    path("multimedia/<int:pk>/image", ListMultimediaImages.as_view()),
+    path("article/image/<int:pk>", ArticleImageDetailView.as_view()),
+    path("multimedia/image/<int:pk>", MultimediaImageDetailView.as_view()),
+    path("multimedia/audio/<int:pk>", MultimediaAudioDetailView.as_view()),
+    path("multimedia/video/<int:pk>", MultimediaVideoDetailView.as_view()),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
