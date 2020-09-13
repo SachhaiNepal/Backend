23aa127dffd93e33844b6c558639ee742fa632a6
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-09-13 23:19:01 +0545

    Added user delete API

--- accounts/views/users.py
+++ accounts/views/users.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from rest_framework import permissions, status
@@ -47,7 +48,7 @@ class UserDetail(APIView):
 
     @staticmethod
     def get_object(pk):
-        return get_object_or_404(User, pk=pk)
+        return get_object_or_404(get_user_model(), pk=pk)
 
     def get(self, request, pk):
         """
@@ -87,10 +88,12 @@ class UserDetail(APIView):
             }, status=status.HTTP_204_NO_CONTENT)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
+    def delete(self, request, pk):
         user = self.get_object(pk)
         user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({
+            "message": "User deleted successfully."
+        }, status=status.HTTP_204_NO_CONTENT)
 
 
 class MemberDetail(APIView):
