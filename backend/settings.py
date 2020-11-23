2500a15dc72e38e9b0d9a92f04a21271bb4633c2
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-11-23 19:27:07 +0545

    Integrated psql database'

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     "branch",
     "multimedia",
     "location",
-    "advertise"
+    "advertise",
+    "event"
 ]
 
 # Rest framework settings
@@ -98,10 +99,20 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME"  : os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME"  : os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
