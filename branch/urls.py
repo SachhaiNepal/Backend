0afa0e246b7b7f510e5bcd6656ad445ff02e69c1
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-11-26 01:09:23 +0545

    Added branch approval toggle API

--- branch/urls.py
+++ branch/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from branch.views.filter import *
-from branch.views.views import BranchViewSet
+from branch.views.views import *
 
 router = DefaultRouter()
 router.register(r'branch', BranchViewSet, basename='branch')
 urlpatterns = router.urls
+
 urlpatterns += [
     path("branch/<int:pk>/member", ListBranchMembers.as_view(), name="branch-members"),
+    path("branch/<int:pk>/toggle-approval", ToggleBranchApprovalView.as_view()),
     path("municipality/<int:pk>/branch", ListMunicipalityBranches.as_view(), name="municipality-branches"),
     path("vdc/<int:pk>/branch", ListVdcBranches.as_view(), name="municipality-branches"),
     path("municipality-ward/<int:pk>/branch", ListMunicipalityWardBranch.as_view(), name="municipality-branches"),
--- branch/views/views.py
+++ branch/views/views.py
@@ -1,5 +1,8 @@
+from coreapi.auth import TokenAuthentication
 from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from branch.serializers import *
 
@@ -20,4 +23,29 @@ class BranchViewSet(viewsets.ModelViewSet):
         branch.delete()
         return Response({
             "message": "Event deleted successfully"
+        }, status=status.HTTP_204_NO_CONTENT)
+
+
+class ToggleBranchApprovalView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    @staticmethod
+    def post(request, pk):
+        try:
+            branch = Branch.objects.get(pk=pk)
+        except Branch.DoesNotExist:
+            return Response({
+                "detail": "Branch does not exist."
+            }, status=status.HTTP_404_NOT_FOUND)
+        branch.is_approved = not branch.is_approved
+        if branch.is_approved:
+            branch.approved_by = request.user
+            branch.approved_at = timezone.now()
+        else:
+            branch.approved_by = None
+            branch.approved_at = None
+        branch.save()
+        return Response({
+            "message": "Branch {} successfully.".format("approved" if branch.is_approved else "rejected")
         }, status=status.HTTP_204_NO_CONTENT)
