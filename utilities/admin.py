5e48483b98adab8a57f2f26e624312bbc4513bd9
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-05-14 23:41:21 +0545

    added required models and apis to app utilities

--- utilities/admin.py
+++ utilities/admin.py
@@ -1,3 +1,107 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
 
-# Register your models here.
+from backend.settings import MAX_SHOWCASE_GALLERY_IMAGES
+from utilities.models import (
+    AboutUs,
+    AboutUsImage,
+    Service,
+    ShowcaseGallery,
+    ShowcaseSlider,
+)
+
+
+class ShowcaseSliderAdmin(admin.ModelAdmin):
+    list_display = ("image", "title", "context", "subtitle", "created_at")
+    search_fields = ("title", "context", "subtitle")
+    list_filter = ["created_at"]
+    date_hierarchy = "created_at"
+    fieldsets = (
+        ("Slider Image", {"classes": ("wide", "extrapretty"), "fields": ("image",)}),
+        (
+            "Slider Image Information",
+            {
+                "classes": ("wide", "extrapretty"),
+                "fields": ("title", "context", "subtitle"),
+            },
+        ),
+    )
+    ordering = ("image", "title", "context", "subtitle")
+
+    def save_model(self, *args, **kwargs):
+        if ShowcaseSlider.objects.count() >= 3:
+            raise ValidationError(
+                "Only {} slider images are allowed to save.".format(3)
+            )
+        super(ShowcaseSliderAdmin, self).save_model(*args, **kwargs)
+
+    def delete_model(self, request, obj):
+        obj.image.delete()
+        obj.delete()
+
+
+class ShowcaseGalleryAdmin(admin.ModelAdmin):
+    search_fields = ("image.name",)
+    list_filter = ["created_at"]
+    date_hierarchy = "created_at"
+    fieldsets = (
+        ("Slider Image", {"classes": ("wide", "extrapretty"), "fields": ("image",)}),
+    )
+    ordering = ("image", "created_at")
+
+    def save_model(self, *args, **kwargs):
+        if ShowcaseGallery.objects.count() >= MAX_SHOWCASE_GALLERY_IMAGES:
+            raise ValidationError(
+                "Only {} images are allowed to save.".format(
+                    MAX_SHOWCASE_GALLERY_IMAGES
+                )
+            )
+        super(ShowcaseGalleryAdmin, self).save_model(*args, **kwargs)
+
+    def delete_model(self, request, obj):
+        obj.image.delete()
+        obj.delete()
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    search_fields = ("title", "description", "image", "created_at")
+    list_filter = ["created_at"]
+    date_hierarchy = "created_at"
+    fieldsets = (
+        (
+            "Service Information",
+            {
+                "classes": ("wide", "extrapretty"),
+                "fields": (
+                    "title",
+                    "description",
+                ),
+            },
+        ),
+        ("Service Media", {"classes": ("wide", "extrapretty"), "fields": ("image",)}),
+    )
+    ordering = ("title", "created_at")
+
+    def delete_model(self, request, obj):
+        obj.image.delete()
+        obj.delete()
+
+
+class AboutUsImageAdmin(admin.ModelAdmin):
+    def delete_model(self, request, obj):
+        obj.image.delete()
+        obj.delete()
+
+
+class AboutUsAdmin(admin.ModelAdmin):
+    def save_model(self, *args, **kwargs):
+        if AboutUs.objects.count() >= 1:
+            raise ValidationError("Single object model. Multiple instance not allowed")
+        super(AboutUsAdmin, self).save_model(*args, **kwargs)
+
+
+admin.site.register(ShowcaseSlider, ShowcaseSliderAdmin)
+admin.site.register(ShowcaseGallery, ShowcaseGalleryAdmin)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(AboutUs, AboutUsAdmin)
+admin.site.register(AboutUsImage, AboutUsImageAdmin)
--- utilities/models.py
+++ utilities/models.py
@@ -1,3 +1,128 @@
-from django.db import models
+import os
+import random
 
-# Create your models here.
+from django.db import models
+from rest_framework.exceptions import ValidationError
+
+from backend.settings import MAX_UPLOAD_IMAGE_SIZE
+
+
+def upload_showcase_slider_image_to(instance, filename):
+    _, file_extension = os.path.splitext(filename)
+    filename = str(random.getrandbits(64)) + file_extension
+    return f"utilities/showcase-slider/{filename}"
+
+
+def upload_showcase_gallery_image_to(instance, filename):
+    _, file_extension = os.path.splitext(filename)
+    filename = str(random.getrandbits(64)) + file_extension
+    return f"utilities/showcase-gallery/{filename}"
+
+
+def upload_services_images_to(instance, filename):
+    _, file_extension = os.path.splitext(filename)
+    filename = str(random.getrandbits(64)) + file_extension
+    return f"utilities/services/{filename}"
+
+
+def upload_about_us_images_to(instance, filename):
+    _, file_extension = os.path.splitext(filename)
+    filename = str(random.getrandbits(64)) + file_extension
+    return f"utilities/about-us/{filename}"
+
+
+class ShowcaseSlider(models.Model):
+    image = models.ImageField(upload_to=upload_showcase_slider_image_to)
+    title = models.TextField(max_length=512, null=True, blank=True)
+    context = models.TextField(max_length=512, null=True, blank=True)
+    subtitle = models.TextField(max_length=1024, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Showcase Slider"
+        verbose_name_plural = "Showcase Slider"
+
+    def clean(self):
+        if self.image and self.image.size / 1000 > MAX_UPLOAD_IMAGE_SIZE:
+            raise ValidationError("Image size exceeds max image upload size.")
+
+    def delete(self, using=None, keep_parents=False):
+        if self.image:
+            self.image.delete()
+        super().delete(using, keep_parents)
+
+
+class ShowcaseGallery(models.Model):
+    image = models.ImageField(upload_to=upload_showcase_gallery_image_to)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Showcase Gallery"
+        verbose_name_plural = "Showcase Gallery"
+
+    def __str__(self):
+        return self.image.name
+
+    def clean(self):
+        if self.image and self.image.size / 1000 > MAX_UPLOAD_IMAGE_SIZE:
+            raise ValidationError("Image size exceeds max image upload size.")
+
+    def delete(self, using=None, keep_parents=False):
+        if self.image:
+            self.image.delete()
+        super().delete(using, keep_parents)
+
+
+class Service(models.Model):
+    image = models.ImageField(upload_to=upload_services_images_to)
+    title = models.CharField(max_length=16)
+    description = models.TextField(max_length=1024)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Services"
+        verbose_name_plural = "Services"
+
+    def __str__(self):
+        return self.title
+
+    def clean(self):
+        if self.image and self.image.size / 1000 > MAX_UPLOAD_IMAGE_SIZE:
+            raise ValidationError("Image size exceeds max image upload size.")
+
+    def delete(self, using=None, keep_parents=False):
+        if self.image:
+            self.image.delete()
+        super().delete(using, keep_parents)
+
+
+class AboutUs(models.Model):
+    about_us = models.TextField(max_length=10000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "About Us"
+
+    class Meta:
+        verbose_name = "About Us"
+        verbose_name_plural = "About Us"
+
+
+class AboutUsImage(models.Model):
+    image = models.ImageField(upload_to=upload_about_us_images_to)
+    about_us = models.ForeignKey(
+        AboutUs, on_delete=models.CASCADE, related_name="about_us_images"
+    )
+
+    def __str__(self):
+        return self.image.name
+
+    def delete(self, using=None, keep_parents=False):
+        if self.image:
+            self.image.delete()
+        super().delete(using, keep_parents)
