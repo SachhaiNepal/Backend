ceb500b7346f8e5c40d1d02d8abf85e0ee386be3
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-05-22 18:15:21 +0545

    Fixed branch as foreign key for member role

--- accounts/sub_models/member_branch.py
+++ accounts/sub_models/member_branch.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ValidationError
 from django.db import models
 
 from accounts.sub_models.member import Member
@@ -9,7 +8,7 @@ class MemberBranch(models.Model):
     member = models.ForeignKey(
         Member, on_delete=models.DO_NOTHING, related_name="member_branches"
     )
-    branch = models.PositiveBigIntegerField()
+    branch = models.ForeignKey(Branch, models.CASCADE, related_name="member_branch")
     date_of_membership = models.DateField()
 
     class Meta:
@@ -18,12 +17,4 @@ class MemberBranch(models.Model):
         unique_together = ("member", "branch")
 
     def __str__(self):
-        return self.member.user.username
-
-    def clean(self):
-        selected_branch = self.branch
-        # check if selected branch id is valid
-        try:
-            Branch.objects.get(pk=selected_branch)
-        except Branch.DoesNotExist:
-            raise ValidationError("Selected branch does not exist.")
+        return "{}: {}".format(self.member.user.username, self.branch.name)
--- accounts/sub_models/member_role.py
+++ accounts/sub_models/member_role.py
@@ -1,9 +1,8 @@
-from django.core.exceptions import ValidationError
 from django.db import models
+from django.core.exceptions import ValidationError
+
 
 from accounts.sub_models.member import Member
-from accounts.sub_models.member_branch import MemberBranch
-from branch.models import Branch
 from utils.constants import MEMBER_ROLE_CHOICES
 
 
@@ -14,29 +13,20 @@ class MemberRole(models.Model):
     role_name = models.CharField(max_length=18, choices=MEMBER_ROLE_CHOICES)
     from_date = models.DateField()
     to_date = models.DateField()
-    branch = models.PositiveBigIntegerField()
+    member_branch = models.ForeignKey("MemberBranch", on_delete=models.CASCADE, related_name="member_branch_roles")
 
     class Meta:
         verbose_name = "Member Role"
         verbose_name_plural = "Member Roles"
-        unique_together = ("member", "role_name", "branch")
+        unique_together = ("member", "role_name", "member_branch")
 
     def clean(self):
-        selected_member = self.member
-        selected_branch = self.branch
-        # check if selected branch id is valid
-        try:
-            Branch.objects.get(pk=selected_branch)
-        except Branch.DoesNotExist:
-            raise ValidationError("Selected branch does not exist.")
-        # check if member is registered in selected branch
-        member_branches = MemberBranch.objects.filter(member=selected_member)
-        found = False
-        for member_branch in member_branches:
-            if member_branch.branch == selected_branch:
-                found = True
-        if not found:
-            raise ValidationError("Member not registered in selected branch.")
+        if self.member_branch.date_of_membership > self.from_date:
+            raise ValidationError("Role period should be within the date of membership.")
+        if self.from_date > self.to_date or self.from_date == self.to_date:
+            raise ValidationError("To date must be smaller than from date.")
+        if self.member_branch.member != self.member:
+            raise ValidationError("Please assign member branch of the same member.")
 
     def __str__(self):
-        return self.member.user.username
+        return "{}: {}".format(self.member.user.username, self.role_name)
