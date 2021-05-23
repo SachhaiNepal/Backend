e59d0f06e2c1b38a45e0c96ba3e08e0c536ba9bf
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-05-23 23:04:06 +0545

    Added new set of migrations

--- event/models.py
+++ event/models.py
@@ -1,2 +1,3 @@
-from event.sub_models import event
-from event.sub_models import event_media
+from event.sub_models.event import *
+from event.sub_models.event_action import *
+from event.sub_models.event_media import *
