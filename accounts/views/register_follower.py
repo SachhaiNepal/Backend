fd061dff20d025449891daa9155ab68d34465235
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-06-01 18:22:14 +0545

    accounts update

--- accounts/views/register_follower.py
+++ accounts/views/register_follower.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,31 +12,31 @@ class RegisterFollower(APIView):
     permission_classes = ()
 
     @staticmethod
-    def post(request, get_user_model=None):
+    def post(request):
         serializer = RegisterFollowerSerializer(data=request.data)
         if serializer.is_valid():
             validated_data = serializer.data
             new_follower = get_user_model().objects.create(
-                username=validated_data["username"],
-                email=validated_data["email"],
-                first_name=validated_data["first_name"],
-                last_name=validated_data["last_name"],
+                username=validated_data.get("username"),
+                email=validated_data.get("email"),
+                first_name=validated_data.get("first_name"),
+                last_name=validated_data.get("last_name"),
             )
             new_follower.set_password(validated_data.pop("confirm_password"))
             new_follower.save()
             # update profile qualities
-            new_follower.profile.contact = validated_data["contact"]
-            new_follower.profile.birth_date = validated_data["birth_date"]
-            new_follower.profile.current_city = validated_data["current_city"]
-            new_follower.profile.home_town = validated_data["home_town"]
+            new_follower.profile.contact = validated_data.get("contact")
+            new_follower.profile.birth_date = validated_data.get("birth_date")
+            new_follower.profile.current_city = validated_data.get("current_city")
+            new_follower.profile.home_town = validated_data.get("home_town")
             new_follower.profile.country = Country.objects.get(
-                id=validated_data["country"]
+                id=validated_data.get("country")
             )
             new_follower.profile.province = Province.objects.get(
-                id=validated_data["province"]
+                id=validated_data.get("province")
             )
             new_follower.profile.district = District.objects.get(
-                id=validated_data["district"]
+                id=validated_data.get("district")
             )
             new_follower.profile.save()
 
