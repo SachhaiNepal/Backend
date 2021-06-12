ae503f206ebb74b141ea0dd14712d7c22b9a3150
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-06-12 11:42:43 +0545

    Separated location constants

---
+++ utils/countries.py
@@ -0,0 +1,10 @@
+COUNTRIES = (
+    ("AFG", "Afghanistan"),
+    ("IND", "India"),
+    ("PAK", "Pakistan"),
+    ("BAN", "Bangladesh"),
+    ("SRI", "Sri Lanka"),
+    ("NEP", "Nepal"),
+    ("BHU", "Bhutan"),
+    ("MAL", "Maldives"),
+)
---
+++ utils/provinces.py
@@ -0,0 +1,9 @@
+PROVINCES = (
+    ("Nepal", "Province No. 1", 1),
+    ("Nepal", "Province No. 2", 2),
+    ("Nepal", "Bagmati Pradesh", 3),
+    ("Nepal", "Gandaki Pradesh", 4),
+    ("Nepal", "Province No. 5", 5),
+    ("Nepal", "Karnali Pradesh", 6),
+    ("Nepal", "Sudurpashchim Pradesh", 7),
+)
