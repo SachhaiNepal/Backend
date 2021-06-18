ac02252375c0c113e596fa28f09a0fd6fe7373a1
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-06-18 23:15:58 +0545

    Added guincorn and allow server

--- backend/settings.py
+++ backend/settings.py
@@ -22,6 +22,8 @@ ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
+    "http://sachchaikendranepal.com",
+    "http://sachchaikendranepal.org.np",
 ]
 
 # Application definition
