accce218990106fbeb7e55c640a0c2b75fb9f98e
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-06-24 23:32:22 +0545

    Added profile location validation for API

--- accounts/serializers/profile.py
+++ accounts/serializers/profile.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from accounts.models import Profile, ProfileImage
 from accounts.sub_models.profile import CoverImage
@@ -30,3 +31,18 @@ class ProfilePOSTSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = "__all__"
+
+    def validate(self, attrs):
+        vdc = attrs.get("vdc")
+        municipality = attrs.get("municipality")
+        vdc_ward = attrs.get("vdc_ward")
+        municipality_ward = attrs.get("municipality_ward")
+        if (vdc and municipality) or (vdc_ward and municipality_ward):
+            raise ValidationError(
+                "Both municipality and vdc fields cannot be selected."
+            )
+        elif municipality and vdc_ward:
+            raise ValidationError("Cannot assign vdc ward for a municipality.")
+        elif vdc and municipality_ward:
+            raise ValidationError("Cannot assign municipality ward for a vdc.")
+        return attrs
