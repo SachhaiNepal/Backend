3d3321179d51085e0c22e448bb062e619488b8a3
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-07-05 18:34:11 +0545

    Updated pagination size to 15

--- backend/settings.py
+++ backend/settings.py
@@ -76,7 +76,7 @@ REST_FRAMEWORK = {
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 3,
+    "PAGE_SIZE": 15,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
